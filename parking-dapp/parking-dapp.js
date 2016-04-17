06e7257af7aca75e7b32af06df92fb2d61699b46
Björn Lakeberg <bjoern.lakeberg@technik-emden.de>
2016-04-17 23:49:29 +0200

    load contract, load places from contract storage, add marker client event with slot info

--- parking-dapp/parking-dapp.js
+++ parking-dapp/parking-dapp.js
@@ -2,46 +2,20 @@ if (Meteor.isClient) {
   const MAP_ZOOM = 15;
   const CENTER = {lat: 53.143722, lng: 8.214059};
   const TIMEOUT_ANIMATION = 200;
+  const CONTRACT_ADDRESS = "0xded0a941b130e7617b5a3464cd43eab52e1f6793";
+  const CONTRACT_ABI = [{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"getSlotCount","outputs":[{"name":"count","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"},{"name":"amount","type":"uint256"}],"name":"addSlots","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"existsPlace","outputs":[{"name":"exists","type":"bool"}],"type":"function"},{"constant":false,"inputs":[],"name":"close","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"atBlock","type":"uint256"}],"name":"getFreeSlotCount","outputs":[{"name":"count","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"atBlock","type":"uint256"},{"name":"toBlock","type":"uint256"}],"name":"calculateEstimatedCosts","outputs":[{"name":"costs","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"blockCosts","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"atBlock","type":"uint256"}],"name":"getNextFreeSlot","outputs":[{"name":"block","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"},{"name":"name","type":"string"},{"name":"lat","type":"string"},{"name":"long","type":"string"}],"name":"addPlace","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"parker","type":"address"}],"name":"getReservedBlock","outputs":[{"name":"block","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"places","outputs":[{"name":"owner","type":"address"},{"name":"name","type":"string"},{"name":"latitude","type":"string"},{"name":"longitude","type":"string"}],"type":"function"},{"constant":false,"inputs":[{"name":"owner","type":"address"},{"name":"time","type":"uint256"}],"name":"reserveSlot","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"controller","outputs":[{"name":"","type":"address"}],"type":"function"},{"inputs":[{"name":"_blockCosts","type":"uint256"}],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"place","type":"address"},{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"latitude","type":"string"},{"indexed":false,"name":"longitude","type":"string"}],"name":"PlaceAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"place","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"SlotsAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"place","type":"address"},{"indexed":false,"name":"parker","type":"address"},{"indexed":false,"name":"reservedBlock","type":"uint256"}],"name":"Reservation","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"to","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"Transaction","type":"event"}];
   
   if(typeof web3 === 'undefined') {
     web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
   }
   EthBlocks.init();
   /*
-  1: add event on each marker for hover to show dummy info
-  2. add blockchain info with block number and block time (allways updating)
   3: read contract storage for count places
-  4: loop
-     for (int i = 0; i < count-1; i++) { 
-       address = places(i)[0];
-       name = places(i)[1];
-       lat = places(i)[2];
-       lng = places(i)[3];
-     }
-  5: add marker for each place
   6: add event for adding a place to add a new marker
-  7: switch from dummy info on (1) to slot info [all: | free: | next free: ]
   */
-  var places = []; //from contract
-  var markers = []; //same index as places
-  places.push({name: "Caecilienschule, Haarenufer", lat: "53.1411913", lng: "8.2013645"});
-  places.push({name: "Herbartgymnasium, Herbartstrasse", lat: "53.140098", lng: "8.206351"});
-  places.push({name: "Theater", lat: "53.138848", lng: "8.210621"});
-  places.push({name: "Theaterwall", lat: "53.137521", lng: "8.211860"});
-  places.push({name: "Kasinoplatz", lat: "53.138116", lng: "8.213750"});
-  places.push({name: "Schlossplatz", lat: "53.137700", lng: "8.214797"});
-  places.push({name: "Damm", lat: "53.135705", lng: "8.217888"});
-  places.push({name: "Am Festungsgraben", lat: "53.136117", lng: "8.219527"});
-  places.push({name: "Kanalstrasse, unter der Amalienbruecke", lat: "53.138573", lng: "8.224472"});
-  places.push({name: "Stautor", lat: "53.140280", lng: "8.217801"});
-  places.push({name: "Kaiserstrasse", lat: "53.141645", lng: "8.221610"});
-  places.push({name: "Rosenstrasse", lat: "53.142636", lng: "8.218853"});
-  places.push({name: "Bahnhof Sued", lat: "53.143749", lng: "8.221406"});
-  places.push({name: "Bahnhof Nord, Willy-Brandt-Platz", lat: "53.145114", lng: "8.222248"});
-  places.push({name: "Am Pferdemarkt", lat: "53.146410", lng: "8.212653"});
-  places.push({name: "91er Strasse, unter Eisenbahnbruecke", lat: "53.145592", lng: "8.212663"});
-  places.push({name: "Bruederstrasse", lat: "53.145527", lng: "8.209240"});
-  places.push({name: "Katharinenstrasse, beim PFL", lat: "53.143351", lng: "8.207896"});
+  var parkingplaces = web3.eth.contract(CONTRACT_ABI).at(CONTRACT_ADDRESS);
+  var markers = []; 
+  var placeInfos = [];
 
   Meteor.startup(function() {  
     GoogleMaps.load();
@@ -69,21 +43,37 @@ if (Meteor.isClient) {
   
   Template.map.onCreated(function() {  
     GoogleMaps.ready('map', function(map) {
-      
-      for (var i = 0; i < places.length; i++) {
-        addMarkerWithTimeout(map, places[i], i * TIMEOUT_ANIMATION);
+      for (var i = 0; i < 18; i++) {
+        addMarkerWithTimeout(map, parkingplaces.places(i), i * TIMEOUT_ANIMATION);
       }
     });
   });
   
   function addMarkerWithTimeout(map, place, timeout) {
     window.setTimeout(function() {
-      markers.push(new google.maps.Marker({
-        title: place.name,
-        position: {lat: Number(place.lat), lng: Number(place.lng)},
+      var marker = new google.maps.Marker({
+        title: place[1],
+        position: {lat: Number(place[2]), lng: Number(place[3])},
         map: map.instance,
         animation: google.maps.Animation.DROP
-      }));
+      });
+      markers.push(marker);
+      addMarkerInfo(map, place, marker);
     }, timeout);
+  }
+  
+  function addMarkerInfo(map, place, marker) {
+    var slotInfo = 
+      '<li><b>owner:          </b>' + place[0] + '</li>' +
+      '<li><b>slots total:    </b>' + parkingplaces.getSlotCount(place[0]) + '</li>' +
+      '<li><b>slots free:     </b>' + parkingplaces.getFreeSlotCount(place[0], EthBlocks.latest.number) + '</li>' +
+      '<li><b>next free slot: </b>' + parkingplaces.getNextFreeSlot(place[0], EthBlocks.latest.number) + '</li>'
+    var infowindow = new google.maps.InfoWindow({
+      content: slotInfo
+    });
+    placeInfos.push(infowindow);
+    marker.addListener('click', function() {
+      infowindow.open(map.instance, marker);
+    });
   }
 }
