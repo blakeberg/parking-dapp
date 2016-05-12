cb95b9dc9cd6db1898125def933146b77b3877aa
Björn Lakeberg <bjoern.lakeberg@technik-emden.de>
2016-05-12 21:00:27 +0200

    POC1 added

--- prototypes/poc1/poc1.js
+++ prototypes/poc1/poc1.js
@@ -1,4 +1,27 @@
 if (Meteor.isClient) {
 
+    Template.dapp.helpers({
+        currentBlockNumber: function () {
+            return "XXXXXX (BigNumber)";
+        },
+        currentBlockTime: function () {
+            return "XX:XX:XX (formatted UNIX Timestamp)";
+        }
+    });
+
+    Template.dapp.events({
+        'click .dapp-block-button'(event) {
+            event.preventDefault();
+            console.log("click event for .dapp-block-button")
+        },
+        'click .dapp-large'(event) {
+            event.preventDefault();
+            console.log("click event for .dapp-large");
+        },
+        'change .block'(event) {
+            event.preventDefault();
+            console.log("change event for .block");
+        }
+    });
 }
 
