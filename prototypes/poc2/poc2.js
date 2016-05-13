09b4af31a614694588aab30a511abb1c4285679f
Björn Lakeberg <bjoern.lakeberg@technik-emden.de>
2016-05-13 21:25:10 +0200

    add POC2

--- prototypes/poc2/poc2.js
+++ prototypes/poc2/poc2.js
@@ -1,22 +1,28 @@
+//single page application (SPA) needs access to google maps  and running ethereum client
 if (Meteor.isClient) {
-
-  var block;
+  //rpc address of ethereum client
+  const ETH_RPC_ADDRESS = 'http://localhost:8545';
+  
   var eventlogs = [];
+  //selected block to estimate or parking
+  var block;
   eventlogs.push("entry1");
   eventlogs.push("entry2");
   eventlogs.push("...");
 
-  if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  //initialize web3 and address of json rpc api from running ethereum client
+  if (typeof web3 === 'undefined') {
+    web3 = new Web3(new Web3.providers.HttpProvider(ETH_RPC_ADDRESS));
   }
 
+  //call when meteor client starting
   Meteor.startup(function () {
+    //EthBlocks with last 50 block information auto updating
     EthBlocks.init();
     EthAccounts.init();
   });
 
+  //template for block and time information
   Template.dapp.helpers({
     currentBlockNumber: function () {
       return EthBlocks.latest.number;
@@ -41,8 +47,10 @@ if (Meteor.isClient) {
     }
   });
 
+  //handle events from dapp template
   Template.dapp.events({
     'click .dapp-block-button'(event) {
+      // Prevent default browser form submit
       event.preventDefault();
       var to = TemplateVar.getFrom('.to .dapp-address-input', 'value');
       EthElements.Modal.question({
@@ -54,17 +62,24 @@ if (Meteor.isClient) {
       });
     },
     'click .dapp-large'(event) {
+      // Prevent default browser form submit
       event.preventDefault();
       var to = TemplateVar.getFrom('.to .dapp-address-input', 'value');
       showMessage("click .dapp-large", "click event for .dapp-large (account choosen: " + to + ")");
     },
     'change .block'(event) {
+      // Prevent default browser form submit
       event.preventDefault();
       block = event.target.value;
       showMessage("change .block", "change event for .block (block input: " + block + ")");
     }
   });
 
+  /**
+   * Formatting of an unix timestamp to a 'hh:mm:ss' string
+   * @param timestamp the unix timestamp of a block in seconds
+   * @returns {string} time formatted as hh:mm:ss
+   */
   function formatTS(timestamp) {
     var date = new Date(timestamp * 1000);
     var hours = date.getHours();
@@ -73,6 +88,11 @@ if (Meteor.isClient) {
     return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
   }
 
+  /**
+   * Shows a modal dialog with header and message
+   * @param header the header of a modal dialog
+   * @param message the message of a modal dialag
+   */
   function showMessage(header, message) {
     EthElements.Modal.show({
       template: 'modal_info',
